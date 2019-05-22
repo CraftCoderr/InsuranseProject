2d0ab5098d3cc0c02145db45d2b951de7f55e3e6
CraftCoderr <n-klyshko@mail.ru>
2019-05-22 08:15:20 +0300

    Fixed CreateClientTest, added CreateContractsTest

---
+++ src/test/java/ru/craftcoderr/tcpp/insproject/test/CreateClientTest.kt
@@ -0,0 +1,33 @@
+package ru.craftcoderr.tcpp.insproject.test
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import ru.craftcoderr.tcpp.insproject.ClientRepository
+import ru.craftcoderr.tcpp.insproject.ContractRepository
+import ru.craftcoderr.tcpp.insproject.OpertorService
+
+class CreateClientTest {
+
+    companion object {
+        lateinit var service : OpertorService
+        lateinit var clientRepository: ClientRepository
+        lateinit var contractRepository: ContractRepository
+
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            clientRepository = mock()
+            contractRepository = mock()
+            service = OpertorService(clientRepository, contractRepository)
+        }
+    }
+
+    @Test
+    fun test_addClient() {
+        val client = service.addClient("Test", "123456", "test@mail.com")
+        verify(clientRepository).addClient(client)
+    }
+
+}
---
+++ src/test/java/ru/craftcoderr/tcpp/insproject/test/CreateContractsTest.kt
@@ -0,0 +1,70 @@
+package ru.craftcoderr.tcpp.insproject.test
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import ru.craftcoderr.tcpp.insproject.ClientRepository
+import ru.craftcoderr.tcpp.insproject.ContractRepository
+import ru.craftcoderr.tcpp.insproject.OpertorService
+import ru.craftcoderr.tcpp.insproject.core.*
+
+class CreateContractsTest {
+
+    lateinit var service : OpertorService
+
+    lateinit var client: Client
+    lateinit var clientRepository: ClientRepository
+    lateinit var contractRepository: ContractRepository
+
+    @BeforeEach
+    fun init() {
+        client = Client("Test", "123456", "test@mail.com")
+        clientRepository = mock<ClientRepository>{
+            whenever(mock.getClient("123456")).thenReturn(client)
+        }
+        contractRepository = mock()
+        service = OpertorService(clientRepository, contractRepository)
+    }
+
+    @Test
+    fun test_CarContract() {
+        val contract = service.createCarInsContract(
+            "123456",
+            System.currentTimeMillis(),
+            Car("testModel", 2012, "a123бв92", 140, "SHBF21742BF624Bf")
+        )
+        assertTrue(contract.premium > 0)
+        verify(contractRepository).addContract(contract)
+        verify(clientRepository).addContract(client, contract)
+    }
+
+    @Test
+    fun test_PersonContract() {
+        val contract = service.createPersonInsContract(
+            "123456",
+            1,
+            System.currentTimeMillis(),
+            Person("Person", GenderType.MALE, 25, true)
+        )
+        assertTrue(contract.premium > 0)
+        verify(contractRepository).addContract(contract)
+        verify(clientRepository).addContract(client, contract)
+    }
+
+
+    @Test
+    fun test_PropertyContract() {
+        val contract = service.createPropertyInsContract(
+            "123456",
+            System.currentTimeMillis(),
+            Property(1000000, "address st., Moscow city")
+        )
+        assertTrue(contract.premium > 0)
+        verify(contractRepository).addContract(contract)
+        verify(clientRepository).addContract(client, contract)
+    }
+
+}
